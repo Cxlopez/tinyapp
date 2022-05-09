191c41fc05bc73fd8e577a30d084c3181a07d7f1
Cxlopez <cristian4402@hotmail.com>
2022-05-08 22:41:31 -0400

    changed all cookies syntax for session

--- express_server.js
+++ express_server.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-undef */
-const express = require("express");
-const app = express();
 const PORT = 8080;
+
+const cookieSession = require('cookie-session');
+const express = require('express');
+
+const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ["key1, key2"],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 const bcrypt = require('bcryptjs');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
 
 // eslint-disable-next-line func-style
 function generateRandomString() {
@@ -86,7 +95,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const uID = req.cookies["user_id"];
+  const uID = req.session.user_id;
   const userUrls = urlsForUser(uID, urlDatabase);
 
   const templateVars = {
@@ -102,18 +111,18 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  if (!req.cookies["user_id"]) {
+  if (!req.session.user_id) {
     return res.redirect("/login");
   }
   const longURL = req.body.longURL;
   const shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = {longURL, userID: req.cookies["user_id"]};
+  urlDatabase[shortURL] = {longURL, userID: req.session.user_id};
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/login", (req, res) => {
   const templateVars = {
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.user_id]
   };
   res.render("urls_login", templateVars);
 });
@@ -122,7 +131,8 @@ app.post("/login", (req, res) => {
   const user = ifUserExists(req.body.email, users);
   if (user) {
     if (bcrypt.compareSync(req.body.password, user.password)) {
-      res.cookie("user_id", user.userID);
+      // eslint-disable-next-line camelcase
+      req.session.user_id = user.userID;
       res.redirect("/urls");
     } else {
       res.status(403).send("Invalid password.");
@@ -133,35 +143,35 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/update", (req, res) => {
-  if (!req.cookies["user_id"]) {
+  if (!req.session.user_id) {
     res.send("no");
     return;
   }
   const url = urlDatabase[req.params.shortURL];
-  if (req.cookies["user_id"] !== req.cookies["user_id"]) {
+  if (req.session.user_id !== req.session.user_id) {
     res.send("no2");
     return;
   }
 
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   urlDatabase[shortURL] = {longURL, userID};
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (!req.cookies["user_id"]) {
+  if (!req.session.user_id) {
     res.send("No");
     return;
   }
   const url = urlDatabase[req.params.shortURL];
-  if (url.userID !== req.cookies["user_id"]) {
+  if (url.userID !== req.session.user_id) {
     res.send("No2");
     return;
   }
@@ -182,9 +192,9 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  if (req.cookies["user_id"]) {
+  if (req.session.user_id) {
     const templateVars = {
-      user: users[req.cookies["user_id"]]
+      user: users[req.session.user_id]
     };
     res.render("urls_new", templateVars);
   } else {
@@ -196,7 +206,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.user_id]
   };
   res.render("urls_show", templateVars);
 });
@@ -204,7 +214,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get('/register', (req, res) => {
   const templateVars = {
-    user: users[req.cookies["user_id"]]
+    user: users[req.session.user_id]
   };
   res.render('urls_registration', templateVars);
 });
@@ -224,7 +234,8 @@ app.post("/register", (req, res) => {
       email: req.body.email,
       password: hashedPassword
     };
-    res.cookie("user_id", userID);
+    // eslint-disable-next-line camelcase
+    req.session.user_id = userID;
     res.redirect('/urls');
   }
 });
