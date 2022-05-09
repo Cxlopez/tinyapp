299dcf69353538d0613a6f54d654d171497cbd06
Cxlopez <cristian4402@hotmail.com>
2022-05-09 16:01:49 -0400

    moved ifUsers exist to helpers.js and created a helpers test to make sure the function is working with mocha and chai

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const ifUserExists = (email, database) => {
+  for (const user in database) {
+    if (database[user].email === email) {
+      return database[user];
+    }
+  }
+  return undefined;
+};
+
+module.exports = { ifUserExists };
